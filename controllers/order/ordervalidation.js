cc4d39cbb0691c9c064a17efa30e00e070e51671
tasneem youssef <120140649+Nemo97-76@users.noreply.github.com>
2024-01-16 16:54:28 +0200

    Update ordervalidation.js

--- controllers/order/ordervalidation.js
+++ controllers/order/ordervalidation.js
@@ -1,3 +1,14 @@
 import Joi from "joi";
-export const AddOrderSchema = {}
-export const cancelOrderSchema = {}
+export const AddOrderSchema = {
+}
+export const successOrderSchema = {
+params:Joi.object({
+token:Joi.string().required()
+}).required()
+}
+
+export const cancelOrderSchema = {
+params:Joi.object({
+token:Joi.string().required()
+}).required()
+}
