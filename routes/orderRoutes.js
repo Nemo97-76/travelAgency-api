39001205b9690856bc1a60ea1e2aa079bfbfe30e
tasneem youssef <tasneemyoussef61@gmail.com>
2024-01-16 17:49:54 +0200

    order CTRL

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,8 +5,8 @@ import * as validators from '../controllers/order/ordervalidation.js'
 import { asyncHandler } from "../utilies/errorHandling.js";
 import * as OC from "../controllers/order/orderController.js"
 const orderRoutes = express.Router()
-orderRoutes.post("/", isAuth(),  validationCoreFunction(validators.AddOrderSchema)  asyncHandler(OC.AddOrder))
-orderRoutes.post("/success/:token", isAuth(),validationCoreFunction(validators.successOrderSchema) , asyncHandler(OC.successOrder))
+orderRoutes.post("/", isAuth(), validationCoreFunction(validators.AddOrderSchema), asyncHandler(OC.AddOrder))
+orderRoutes.post("/success/:token", isAuth(), validationCoreFunction(validators.successOrderSchema), asyncHandler(OC.successOrder))
 orderRoutes.delete("/cancel/:token", isAuth(), validationCoreFunction(validators.cancelOrderSchema), asyncHandler(OC.cancelOrder))
 
 export default orderRoutes
