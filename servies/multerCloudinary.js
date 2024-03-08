ba2e74da3e0d39fd847884054dc6d7694bb6138c
tasneem youssef <120140649+Nemo97-76@users.noreply.github.com>
2024-03-08 18:06:38 +0200

    Update multerCloudinary.js

--- servies/multerCloudinary.js
+++ servies/multerCloudinary.js
@@ -9,7 +9,7 @@ export const multerCloudFunction = (allowedExtentionsArray) => {
         if (allowedExtentionsArray.includes(file.mimetype)) {
             return cb(null, true)
         }
-        cb(new Error('invalid extention', { cause: 400 }), false)
+        cb(new Error('invalid extention,the allowed extentions are:1=>for vedios (mp4,mkv) ,2=>for images (png,jpeg,gif)', { cause: 400 }), false)
     }
     const fileUpload = multer({
         fileFilter,
